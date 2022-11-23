f8a368c2fd93e4b85eff63c550903ebebce1fd6c
justjundana <jalbasyir@gmail.com>
2022-11-23 21:26:04 +0700

    Initial Commit

---
+++ travel/travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelApp.swift
+//  travel
+//
+//  Created by Jundana Al-Basyir on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
